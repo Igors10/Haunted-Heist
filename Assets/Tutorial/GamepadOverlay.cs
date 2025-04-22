3ba5ef0e3d07bb2db315573456323141116f3a15
Igors <igors.sorogovecs.8360@student.uu.se>
2025-04-22 19:02:45 +0200

    gamepad overlay wip

--- Assets/Tutorial/GamepadOverlay.cs
+++ Assets/Tutorial/GamepadOverlay.cs
@@ -1,13 +1,26 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class GamepadOverlay : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI[] overlays;
     [SerializeField] string[] keyboard_controls;
     [SerializeField] string[] gamepad_controls;
+    [SerializeField] GameObject[] input_ui; 
+    /*
+     * 0- left mouse ability robber
+     * 1- right mouse ability robber
+     * 2- left mouse ability ghost
+     * 3- right mouse ability ghost
+     * 4- picking up items
+     * 
+     * Tutorials have shortened versions
+     */
 
-    bool is_controller_input = false;
+    [SerializeField] Sprite[] gamepad_ui_sprites;
+    [SerializeField] Sprite[] keyboard_ui_sprites;
+ 
     [SerializeField] bool debug_log = false;
 
     /*
@@ -52,7 +65,7 @@ public class GamepadOverlay : MonoBehaviour
             joystick_is_connected = true;
             if (debug_log) Debug.Log(Input.GetAxis("VerticalJoystick"));
         }
-        else if (Input.GetKeyDown(KeyCode.Joystick1Button0)) joystick_is_connected = true;
+        else if (Input.GetKeyDown(KeyCode.Joystick1Button0)) joystick_is_connected = true; // this part doesnt actually trigger
 
         return joystick_is_connected;
     }
@@ -82,12 +95,14 @@ public class GamepadOverlay : MonoBehaviour
             // If controller is connected use the controller input text
             if (debug_log) Debug.Log("Controller connected");
             InitControlsText(gamepad_controls);
+            InitControlVisuals(gamepad_ui_sprites);
         }
         else if (CheckKeyInput())
         {
             // If no controller connected use the keyboard input text
             if (debug_log) Debug.Log("No controller connected.");
             InitControlsText(keyboard_controls);
+            InitControlVisuals(keyboard_ui_sprites);
         }
     }
     
@@ -104,6 +119,15 @@ public class GamepadOverlay : MonoBehaviour
         for (int a = 0; a < control_text.Length; a++)
         {
             overlays[a].text = overlays[a].text.Replace("{key}", control_text[a]);
+        }
+    }
+
+    void InitControlVisuals(Sprite[] control_visuals)
+    {
+        for (int a = 0; a < input_ui.Length; a++)
+        {
+            if (input_ui[a].TryGetComponent(out SpriteRenderer sprite)) sprite.sprite = control_visuals[a];
+            else if (input_ui[a].TryGetComponent(out Image image)) image.sprite = control_visuals[a];
         }
     }
 }
